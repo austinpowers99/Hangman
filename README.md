e5c3d9e768eb8a6fd9974a50ffe60f5ec1b7b626
austinpowers99 <samanthaf_9@outlook.com>
2023-12-04 15:41:45 -0800

    Update README

---
+++ README.md
@@ -0,0 +1,21 @@
+Hangman
+
+<!-- Click the link to play! 👉🏼 [Hangman]() -->
+
+# How to play
+
+
+# Features
+
+
+# Preview 
+<!-- <img width="638" alt="Screenshot 2023-06-26 at 8 05 45 AM" src="https://github.com/austinpowers99/Concentration-Project1/assets/135395052/b002611b-f053-4ff1-ac58-8c518346a422"> -->
+
+# Technologies used
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)
+![CSS3](https://img.shields.io/badge/CSS3-1572B6?style=for-the-badge&logo=css3&logoColor=white)
+
+# Icebox features
+
+<!-- [wireframe](https://excalidraw.com/#json=kOfvkizbydq3aVpiATw7d,0nmXmaf7IWG8Ch7npIyJdw) -->
---
+++ main.js
@@ -0,0 +1,35 @@
+/*----- constants -----*/
+
+
+/*----- state variables -----*/
+
+
+/*----- cached elements  -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+
+
+
+// Code away!
+
+// Start with some HTML in index.html for the basic layout of the UI. If an element's content is going to come from the render function, you may want to temporarily include mocked content in the HTML to help with layout and styling. However, once the content is being provided by the render() function, you should remove the mocked content from index.html.
+
+// Declare, but don't initialize, the application-wide state variables. The initialization of the variables to their "initial" state should be done within an initialize, or similarly named function, e.g., init.
+
+// Write that initialize function.
+
+// Invoke initialize() to "kick off" the app.
+
+// Now that the initialize function has initialized the state variables, the last line in initialize should be render(); to render that state to the DOM for the first time.
+
+// Stub up that render() function.
+
+// As a reminder, after state has been updated in an event listener, the last line of code in the event listener function should be a call to render(); again, to render the state to the DOM.
+
+// Register event listeners - be sure to use event delegation!
+
+// Code the event listener(s). This is where most of the app's logic will exist.
